90398ce935234e22baf8badc893b7063eed0485b
KxSKanade <terry.lazaro@tecsup.edu.pe>
2025-07-10 18:20:01 -0500

    preguntas

--- src/app/preguntas/page.tsx
+++ src/app/preguntas/page.tsx
@@ -23,9 +23,9 @@ const faqs: FAQ[] = [
   {
     question: '¿Puedo cambiar o devolver un producto?',
     answer: `Querida sg lover, tu satisfacción es nuestra prioridad, por ello puedes solicitar un cambio de producto siempre que:\n
-    • este no haya sido utilizado, lavado o esté en su empaque original con todas las etiquetas intactas tal cual lo recibiste.\n
-    • no tenga marcas sucias o de maquillaje.\n
-    • no tenga olor a perfume, desodorante, cosméticos u olor a lavado.\n
+    • Este no haya sido utilizado, lavado o esté en su empaque original con todas las etiquetas intactas tal cual lo recibiste.\n
+    • No tenga marcas sucias o de maquillaje.\n
+    • No tenga olor a perfume, desodorante, cosméticos u olor a lavado.\n
     Revisaremos todos los artículos al recibirlos. cualquier producto que no cumpla con las condiciones anteriores será devuelto al cliente.\n
     En caso de notar que el producto ha sido manipulado, no se podrá realizar el cambio. no aceptamos productos que no estén en las condiciones originales de venta.\n
     Además, el costo del delivery de cambio es responsabilidad del cliente.\n
